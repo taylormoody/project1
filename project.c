e4b32f14fa40d7fcef1f50a413e865b1239b5de7
taylormoody <49775706+taylormoody@users.noreply.github.com>
2019-04-19 17:16:16 +1000

    First encryption attempt type

--- project.c
+++ project.c
@@ -7,9 +7,13 @@
 
 
 //read in with stdin give option for each function with printf//
-//
+//The rotation encryption function//
+char rotationEncryption();
+
 int main() {
     int menuChoice;
+    char encryption;
+    char text[1000];
     printf("Please choose a cipher function\n");
     printf("Press 1 for rotation encryption\n");
     printf("Press 2 for rotation decryption\n");
@@ -20,8 +24,36 @@ int main() {
     printf("Press 6 for advanced substitution decryption\n");
     scanf("%d", &menuChoice);
     
-    if (menuChoice ==1)
+    if (menuChoice ==1){
+        //requests the text to be encrpted and uses the rotationEncryption function//
+        printf("Please input the text to be encrypted in Uppercase\n");
+        scanf("%s", text);
+        printf("Please input Rotation Amount\n")
+        scanf("%d", rotationAmount);
+        
+        encryption=rotationEncryption(text,rotationAmount);
+        printf("%s", encryption);
+    }
         //run = function
-    
+
     return 0;
+}
+char rotationEncryption(){
+    char text[1000],character;
+    int x=0, rotationAmount;
+    //runs a loop from text start to the last character,stopping when it hits the end of the string//
+    //this is why the loop starts at x=0 and uses ++x instead of x++//
+    for (x=0 ; text < '\0'; ++x){
+        //so that the text can be encrypted without things getting weird, baiscally copying it from one//
+        //string to another and then saying the second string is equal to the first//
+        character=text[i];
+        
+        if(character>= 'A' && character<='Z'){
+            character = character + rotationAmount;
+            
+            if (character >'Z' && character <'A'){
+                printf("Please only input uppercase letters\n");
+            }
+    }
+    
 }
