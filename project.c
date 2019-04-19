3b977e45c4cf87596a90f26b527c680f578c8d06
taylormoody <49775706+taylormoody@users.noreply.github.com>
2019-04-19 16:06:44 +1000

    Basic user menu

--- project.c
+++ project.c
@@ -1,3 +1,27 @@
-int main(){
-    printf("reee")
+#include <stdio.h>
+//Assignment 1 eng1003//
+
+//The int main will simply be able to call each function I make//
+//In order to space each function properly//
+
+
+
+//read in with stdin give option for each function with printf//
+//
+int main() {
+    int menuChoice;
+    printf("Please choose a cipher function\n");
+    printf("Press 1 for rotation encryption\n");
+    printf("Press 2 for rotation decryption\n");
+    printf("Press 3 for substitution encryption\n");
+    printf("Press 4 for substitution decryption\n");
+    //For the two cipher only text decryptions//
+    printf("Press 5 for advanced rotation decryption\n");
+    printf("Press 6 for advanced substitution decryption\n");
+    scanf("%d", &menuChoice);
+    
+    if (menuChoice ==1)
+        //run = function
+    
+    return 0;
 }
