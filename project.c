802ad21be5be53858f0495f1a305cea166532b44
taylormoody <49775706+taylormoody@users.noreply.github.com>
2019-04-23 18:39:11 +1000

    part two sadness boogaloo

--- project.c
+++ project.c
@@ -10,6 +10,7 @@
 //read in with stdin give option for each function with printf//
 //The rotation encryption function//
 void rotationEncryption();
+void rotationDecryption();
 
 int main() {
     int menuChoice=0,rotationAmount=0, x=0;
@@ -26,8 +27,12 @@ int main() {
     
     if (menuChoice ==1){
         rotationEncryption(text[x],rotationAmount);
-
     }
+    else if (menuChoice ==2){
+        rotationDecryption(text[x],rotationAmount);
+    }
+
+    
         //run = function
 
     return 0;
@@ -65,6 +70,49 @@ void rotationEncryption(){
             cipher[x++]=(char)c;
         }
         
+    }    
+    printf("%s\n", text);
+    printf("%s\n", cipher);
+
+    
+}
+void rotationDecryption(){
+    char text[1000]={0}, cipher[1000]={0};
+    int rotationAmount, c;
+    printf("Please input Rotation Amount: ");
+    scanf("%d", &rotationAmount);
+    getchar();
+    //this getchar is here because it lets the code work and im not good enough to make a better fix
+    //runs a loop from text start to the last character,stopping when it hits the end of the string//
+    //this is why the loop starts at x=0 and uses ++x instead of x++//
+    printf("Please input Text to be Decrypted in Uppercase: ");
+    while ((c=getchar()) !='\n'){
+        static int x=0,i=0;
+        
+        if (c-rotationAmount>='A' && c-rotationAmount<='Z'){
+            text[i++]=(char)c;
+            cipher[x++]=(char)(c-rotationAmount);
+        }
+        else if (c-rotationAmount>='a' && c-rotationAmount<='z'){
+            c = c-32;
+            text[i++]=(char)c;
+            cipher[x++]=(char)(c-rotationAmount);
+        }
+        else if (c<'A'){
+            text[i++]=(char)c;
+            cipher[x++]=(char)c;
+        }
+        else if (c>'Z' && c<'a'){
+            text[i++]=(char)c;
+            cipher[x++]=(char)c;
+        }
+        else if (c-rotationAmount<'A'){
+            
+            c=c+26;
+            text[i++]=(char)(c-26);
+            cipher[x++]=(char)(c-rotationAmount);
+
+        }
     }    
     printf("%s\n", text);
     printf("%s\n", cipher);
